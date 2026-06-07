1b9b8cdfef14ba628720c3087e9a8047b19fa2e1
sbag@fedora <101804633+Shnku@users.noreply.github.com>
2026-06-07 19:10:42 +0530

    update shizuku functionality

--- adb_automate/disbl_dev_szku.py
+++ adb_automate/disbl_dev_szku.py
@@ -1,6 +1,12 @@
+import time
 import rich
+from rich import progress
 from rich.console import Console
+from rich.live import Live
 from rich.panel import Panel
+from rich.progress import BarColumn, Progress, TextColumn, track
+from rich.prompt import Prompt
+from rich.table import Column
 from rich.text import Text
 from rich.markdown import Markdown
 from functionality.adb import ADB_COMMANDS, list_devices
@@ -23,12 +29,24 @@ def install_drivers():
             install_package(package)
 
 
+# def connect_device():
+#     has_device = False
+#     while not has_device:
+#         output = list_devices()
+#         if "device" in output:
+#             has_device = True
+
+
 def connect_device():
     has_device = False
     while not has_device:
         output = list_devices()
-        if "device" in output:
+        yield f"{output}"  # Yield the current output for live updates
+
+        if "devicerrt" in output:
             has_device = True
+
+    yield "Device connected."
 
 
 def main():
@@ -52,6 +70,39 @@ def main():
 
 
 if __name__ == "__main__":
-    install_drivers()
-    connect_device()
-    main()
+    # install_drivers()
+    # connect_device()
+    # main()
+
+    m = Markdown(
+        """
+    
+# Initial Preperation  
+
+Connect your Android Phone  
+Goto Developer Optiones -> enable "USB Debugging"  
+Click allow in the popup window (always allow)  
+    """
+    )
+
+p = Panel(
+    m,
+)
+console.print(p)
+
+c = Prompt.ask("Would you like to proceed", choices=["y", "n"])
+# console.print(Panel(Prompt.ask("Would you like to proceed", choices=["y", "n"])))
+
+
+generator = connect_device()
+connect_device()
+with Live(console=console, refresh_per_second=4) as live:
+    for message in generator:
+        # Each iteration updates the Live display
+        live.update(message)
+with Progress("package"):
+    for n in progress.track(sequence=range(100), description="jh"):
+        # progress.print(n)
+        time.sleep(0.1)
+
+# with Progress(connect_device()):
